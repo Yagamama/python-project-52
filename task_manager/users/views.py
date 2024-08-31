c2e6871fcd2129196dd5cc149871510319295ebc
Sergeeva Larisa <mindzu@mail.ru>
2024-09-01 01:01:12 +0500

    user CRUD is almost there

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render, redirect
 from django.views import View
 from .forms import UserForm
 from .models import Users
+from django.utils.translation import gettext_lazy
+import re
+
+ERR_DIFFERENT_PASS = gettext_lazy('Введенные пароли не совпадают.')
+ERR_SHORT_PASS = gettext_lazy('Пароль слишком короткий.')
+BTN_CREATE = gettext_lazy('Зарегистрировать')
+BTN_UPDATE = gettext_lazy('Сохранить')
 
 # def index(request):
 #     return render(request, 'users.html')
@@ -9,38 +16,85 @@ from .models import Users
 class UsersView(View):
 
     def get(self, request, *args, **kwargs):
-        return render(request, 'users.html')
+        u = Users.objects.all().order_by('created_at')
+        return render(request, 'users.html', {'users': u})
     
 
 class UserCreateView(View):
 
     def get(self, request, *args, **kwargs):
-        form = UserForm()
-        return render(request, 'u_create.html', {'form': form})
+        return render(request, 'u_create.html', {'user': request.POST or None, 'btn': BTN_CREATE})
     
     def post(self, request, *args, **kwargs):
-        if validate_user(**kwargs):
-            return redirect('/users/')
-        return render(request, 'users.html')
+        # form = UserForm(request.POST)
+        # if form.is_valid():
+        #     errors = user_errors(request.POST)
+        #     if not errors:
+        #         form.save()
+        #         return redirect('/users/')
+        #     return render(request, 'u_create.html', {'form': form, 'errors': errors})
+        errors = user_errors(request.POST)
+        if errors:
+            return render(request, 'u_create.html', {'user': request.POST, 'errors': errors, 'btn': BTN_CREATE})
+        save_data(request.POST)
+        return redirect('/users/')
     
 
 class UserUpdateView(View):
 
     def get(self, request, *args, **kwargs):
-        return render(request, 'u_create.html')
+        id = kwargs.get('pk')
+        user = Users.objects.get(id=id)
+        return render(request, 'u_create.html', {'user': user, 'btn': BTN_UPDATE})
     
     def post(self, request, *args, **kwargs):
-        return render(request, 'users.html')
+        id = kwargs.get('pk')
+        user = Users.objects.get(id=id)
+        errors = user_errors(request.POST)
+        if errors:
+            return render(request, 'u_create.html', {'user': request.POST, 'errors': errors, 'btn': BTN_UPDATE})
+        save_data(user, id=request.POST.get('id'))
+        return redirect('/users/')
     
 
 class UserDeleteView(View):
 
     def get(self, request, *args, **kwargs):
-        return render(request, 'user_delete.html')
+        id = kwargs.get('pk')
+        user = Users.objects.get(id=id)
+        return render(request, 'user_delete.html', {'user': user})
     
     def post(self, request, *args, **kwargs):
-        return render(request, 'users.html')
+        id = kwargs.get('pk')
+        user = Users.objects.get(id=id)
+        user.delete()
+        return redirect('/users/')
 
 
-def validate_user(**kwargs):
-    return True
+def user_errors(data):
+    errors = []
+    pas = data.get('password1', '')
+    pas2 = data.get('password2', '')
+    username = data.get('username', '')
+    if len(username) > 150:
+        errors.append('too_long_name')
+    inv_chars = re.search('\W[^@.+-]', username)
+    if inv_chars:
+        errors.append('invalid_chars')
+    if len(pas) < 3:
+        errors.append('too_short_pass')
+    if pas != pas2:
+        errors.append('different_passes')
+    return errors
+
+
+def save_data(data, id=0):
+    if id > 0:
+        u = Users.objects.get(id=data.get('id'))
+    else:
+        u = Users()
+    u.first_name = data.get('first_name')
+    u.last_name = data.get('last_name')
+    u.username = data.get('username')
+    u.password = data.get('password1')
+    u.save()
